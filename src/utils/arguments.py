d2a352ddece6e1d27132e0014a016d052e3c7293
MinaAlmasi <202005465@post.au.dk>
2023-10-15 10:32:42 +0200

    make short arg

--- src/utils/arguments.py
+++ src/utils/arguments.py
@@ -5,6 +5,6 @@ import argparse
 
 def input_parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--recording', type=int, default=0, help="Number of the recording to be used")
+    parser.add_argument('--recording', '-r', type=int, default=0, help="Number of the recording to be used")
     args = parser.parse_args()
     return args
