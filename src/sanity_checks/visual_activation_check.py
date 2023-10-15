ecd1d3ba386002361b6cbe8567e08c3fa8a6978f
MinaAlmasi <202005465@post.au.dk>
2023-10-15 10:08:54 +0200

    working plots (with evokeds)

--- src/sanity_checks/visual_activation_check.py
+++ src/sanity_checks/visual_activation_check.py
@@ -1,5 +1,8 @@
 '''
 Sanity check for visual activation.
+
+Run in terminal: 
+    python src.sanity_checks.visual_activation_check -r 0
 '''
 # utils 
 import pathlib 
@@ -10,24 +13,28 @@ import mne
 
 # custom module
 from src.utils.general_preprocess import preprocess, epoching, ica_dict, create_evoked
+from src.utils.arguments import input_parse
 
 def main(): 
+    # args
+    args = input_parse()
+
     # define paths 
     path = pathlib.Path(__file__)
 
     meg_path = path.parents[4] / "834761" / "0108" / "20230928_000000" / "MEG"
     ica_path = path.parents[2] / "data" / "ICA"
 
-    plots_path = path.parents[1] / "plots" / "sanity_checks"
+    plots_path = path.parents[2] / "plots" / "sanity_checks"
     plots_path.mkdir(parents=True, exist_ok=True) # make plots path if it does not exist
 
     # define recording names 
-    recording_names = ['001.self_block1',  '002.other_block1',
-                       '003.self_block2',  '004.other_block2',
-                       '005.self_block3',  '006.other_block3']
+    recording_names = {0: '001.self_block1',  1: '002.other_block1',
+                       2: '003.self_block2',  3: '004.other_block2',
+                       4: '005.self_block3',  5: '006.other_block3'}
 
     # define recording name
-    chosen_recording = recording_names[1]
+    chosen_recording = recording_names[args.recording]
 
     # print ica components to exclude
     ica_components = ica_dict()
@@ -43,8 +50,11 @@ def main():
     # check if self or other block to determine event ids
     if "self" in chosen_recording: 
         event_id = dict(self_positive=11, self_negative=12, button_img=23)
+        # define condition
+        cond = "self"
     else: 
         event_id = dict(other_positive=21, other_negative=22, button_img=23)
+        cond = "other"
 
     # do epochs 
     reject = dict(mag=4e-12, grad=4000e-13) # T, T/m, V
@@ -52,19 +62,17 @@ def main():
     epochs = epoching(processed_raw, events, event_id=event_id, tmin=-0.200, tmax=1.000, reject_criterion=reject)
     print(epochs)
 
-    plot = epochs.plot_image(picks="meg", combine="mean")
-
-    # resample to 250 Hz
-    epochs = epochs.resample(250)
+    plots = epochs.plot_image(picks="meg", combine="mean")
 
     # save plot
-    plot.savefig(plots_path / "epochs.png")
+    for i, _ in enumerate(plots): 
+        plots[i].savefig(plots_path / f"{chosen_recording[4:]}_epochs_{i}.png")
 
     # compute evokeds
-    evoked = create_evoked(epochs, triggers=['other_positive', 'other_negative', 'button_img'])
+    evoked = create_evoked(epochs, triggers=[f'{cond}_positive', f'{cond}_negative', 'button_img'])
 
-    pos_evoked =  evoked['other_positive']
-    neg_evoked = evoked['other_negative']
+    pos_evoked =  evoked[f'{cond}_positive']
+    neg_evoked = evoked[f'{cond}_negative']
     button_evoked = evoked['button_img']
 
     # combine evoked 
@@ -75,8 +83,8 @@ def main():
     pos_evoked_plot[1].savefig(plots_path / f"all_evoked_plot_{chosen_recording[4:]}.png", dpi=1200)
 
     # plot topographies
-    #pos_evoked_topo_plot = visual_evoked.plot_topomap(times=[-0.1, 0.0, 0.3, 0.8, 1.4], ch_type="grad")
-    #pos_evoked_topo_plot.savefig(plots_path / "pos_evoked_topo_plot.png")
+    pos_evoked_topo_plot = visual_evoked.plot_topomap(times=[-0.2, 0.12, 0.5, 0.82, 1], ch_type="grad")
+    pos_evoked_topo_plot.savefig(plots_path / f"pos_evoked_topo_plot_{chosen_recording[4:]}.png")
 
 
 if __name__ == "__main__":
